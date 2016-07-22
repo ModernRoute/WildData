d53d0c651cf2f56a39533ac2b2397000578e7511
Anton Podavalov <a.podavalov@gmail.com>
2016-07-22 14:26:03 -0700

    Added solution.

---
+++ WildData.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WildData.Test")]
+[assembly: AssemblyDescription(".NET ")]
+[assembly: AssemblyConfiguration(".NET Data Client Tests")]
+[assembly: AssemblyCompany("Modern Route")]
+[assembly: AssemblyProduct("WildData")]
+[assembly: AssemblyCopyright("Copyright © Modern Route 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("654c96a5-d419-48a0-8ed9-4bce247c931e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0.*")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
